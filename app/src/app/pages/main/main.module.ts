0aceb5a9fe3174cb18e528a496deb3c6cac02da9
AliceNov <alisa.nov72@gmail.com>
2022-10-06 00:11:56 +0300

    add style for main page

--- app/src/app/pages/main/main.module.ts
+++ app/src/app/pages/main/main.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main/main.component';
+import { MaterialsModule } from 'src/app/modules/materials/materials.module';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './main/search.pipe';
+import { NgxSpinner, NgxSpinnerComponent, NgxSpinnerModule } from 'ngx-spinner';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    SearchPipe
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MaterialsModule,
+    FormsModule,
+    NgxSpinnerModule
+    
   ]
 })
 export class MainModule { }
--- app/src/app/pages/main/main/main.component.ts
+++ app/src/app/pages/main/main/main.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { map, take, tap } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { takeUntil, tap } from 'rxjs';
+import { RxUnsubscribe } from 'src/app/rx-unsubscribe';
 import { DataService } from 'src/app/services/data/data.service';
+import { FavoriteService } from 'src/app/services/favorite/favorite.service';
 import { IData } from 'src/models/data.inteface';
 
 @Component({
@@ -9,46 +12,75 @@ import { IData } from 'src/models/data.inteface';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.less']
 })
-export class MainComponent implements OnInit {
+export class MainComponent extends RxUnsubscribe implements OnInit {
   
-  list: Array<{ id: string; title: string; }> = [];
-  listOfTitle: Array<string> = [];
-  listOfVideos: Array<string> = [];
+  list: Array<{id: string, title: string, iconName: string}> = [];
   urlSafe: any;
+  icon: string = "bookmark_border";
+  inS: string = '';
   
-  constructor(private dataService: DataService, private sanitizer : DomSanitizer ) {
-
+  constructor(private dataService: DataService,
+              private sanitizer : DomSanitizer,
+              private favoriteService: FavoriteService,
+              private spinner: NgxSpinnerService ) {
+                super();
    }
 
   ngOnInit(): void {
+   /* this.spinner.show()
+    setTimeout(()=> {
+      this.spinner.hide()
+    },10000)*/
     this.uploadingVideos();
   }
 
   uploadingVideos(): void{
     this.dataService.getListOfViodeo().pipe(
+      takeUntil(this.destroy$),
       tap((value) => {
-        this.fullListOfTitle(value);
+        this.list = this.fullListOfTitle(value);
       })
     )
     .subscribe();
   }
 
-  fullListOfTitle(list: IData){
+  fullListOfTitle(list: IData): any{
+    let arr: any = [];
     for(let i = 0; i < list.items.length; i++){
-      this.listOfTitle.push(list.items[i].snippet.title);
-      this.listOfVideos.push(list.items[i].id);
       let video = {
         id: list.items[i].id,
-        title: list.items[i].snippet.title
+        title: list.items[i].snippet.title,
+        iconName: "bookmark_border"
       }
-      this.list.push(video);
+      arr.push(video);
     }
-    console.log(this.list);
-    this.urlSafe = this.sanitizer?.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.listOfVideos[0]}`);
+    return arr;
   }
 
   getSafeUrl(value: string){
     
     return this.sanitizer?.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${value}`);
+  }
+
+  addToFavorite(id: string, title: string, idIcon: number){
+    if (this.list[idIcon].iconName === "bookmark_border"){
+      this.list[idIcon].iconName = "bookmark";
+      this.favoriteService.add(id, title);
+    } else {
+      this.list[idIcon].iconName = "bookmark_border";
+      this.favoriteService.remove(id, title);
+    }
+  }
+
+  loadMore(){
+    this.dataService.getListOfViodeo().pipe(
+      takeUntil(this.destroy$),
+      tap(value => {
+        let arr = this.fullListOfTitle(value);
+        this.list = this.list.concat(arr);
+        console.log(this.list);
+      }) 
+    )
+    .subscribe();
   }
 }
